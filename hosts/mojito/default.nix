cf961a03a01a5f47daf92a906b47d7a40483a9d4
James Behr <jamesbehr@gmail.com>
2023-07-18 14:23:55 +0200

    feat: automate garbage collection

--- hosts/mojito/default.nix
+++ hosts/mojito/default.nix
@@ -98,6 +98,11 @@
     extraOptions = ''
       experimental-features = nix-command flakes
     '';
+    gc = {
+      automatic = true;
+      dates = "weekly";
+      options = "--delete-older-than 30d";
+    };
   };
 
   # Networking
--- hosts/sidecar/default.nix
+++ hosts/sidecar/default.nix
@@ -7,6 +7,11 @@
       extra-platforms = x86_64-darwin
       experimental-features = nix-command flakes
     '';
+    gc = {
+      automatic = true;
+      interval = { Weekday = 0; Hour = 0; Minute = 0; };
+      options = "--delete-older-than 30d";
+    };
   };
 
   services.nix-daemon.enable = true;
