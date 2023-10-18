59b2e6d4df2d7bbe68ea2a3a34fb994157fd3795
James Behr <jamesbehr@gmail.com>
2023-10-18 14:23:04 +0200

    mojito: update to unstable

--- flake.nix
+++ flake.nix
@@ -2,14 +2,14 @@
   description = "James's system configuration";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nur.url = "github:nix-community/NUR";
     darwin = {
       url = "github:lnl7/nix-darwin/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.05";
+      url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
